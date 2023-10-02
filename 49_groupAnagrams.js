a271fde23efedff34d51290bacb4a7271fad688a
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-10-01 23:48:48 -0700

    improved code

--- 49_groupAnagrams.js
+++ 49_groupAnagrams.js
@@ -1,29 +1,50 @@
-/**
- * @param {string[]} strs
- * @return {string[][]}
- */
-var groupAnagrams = function(strs) {
-  let strsSorted = []
+// /**
+//  * @param {string[]} strs
+//  * @return {string[][]}
+//  */
+// var groupAnagrams = function(strs) {
+//   let strsSorted = []
 
-  // sort
-  for (const s of strs) {
-      let sorted = s.split("").sort().join("")
-      strsSorted.push(sorted)
+//   // sort
+//   for (const s of strs) {
+//       let sorted = s.split("").sort().join("")
+//       strsSorted.push(sorted)
+//   }
+
+//   // add to hash
+//   let hashedAnagrams = {}
+//   for (let i=0; i<strs.length; i++) {
+//       if (hashedAnagrams[strsSorted[i]]) {
+//           hashedAnagrams[strsSorted[i]] = [
+//               ...hashedAnagrams[strsSorted[i]],
+//               strs[i]
+//           ]
+//       } else {
+//           hashedAnagrams[strsSorted[i]] = [strs[i]]
+//       }
+//   }
+
+//   return Object.values(hashedAnagrams)
+
+// };
+
+function groupAnagrams(strings) {
+  let groupedStrings = {}
+  for (let i=0; i<strings.length; i++) {
+    let sortedKey = sortString(strings[i])
+    if (!groupedStrings[sortedKey]) {
+      groupedStrings[sortedKey] = []
+    }
+    groupedStrings[sortedKey].push(strings[i])
   }
+  return Object.values(groupedStrings)
+}
 
-  // add to hash
-  let hashedAnagrams = {}
-  for (let i=0; i<strs.length; i++) {
-      if (hashedAnagrams[strsSorted[i]]) {
-          hashedAnagrams[strsSorted[i]] = [
-              ...hashedAnagrams[strsSorted[i]],
-              strs[i]
-          ]
-      } else {
-          hashedAnagrams[strsSorted[i]] = [strs[i]]
-      }
-  }
+function sortString(string) {
+  return string.split("").sort().join("")
+}
 
-  return Object.values(hashedAnagrams)
+// console.log(sortString("eat"))
 
-};
+let strs = ["eat","tea","tan","ate","nat","bat"]
+console.log(groupAnagrams(strs))
