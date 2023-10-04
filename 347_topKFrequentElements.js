4791e3c0ef3f9a2fed6f088feee6116137dbd7fc
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-10-03 23:52:05 -0700

    add working solution for one instance'

--- 347_topKFrequentElements.js
+++ 347_topKFrequentElements.js
@@ -4,7 +4,42 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    
+    let numsFrequency = {}
+    for (let i=0; i<nums.length; i++) {
+      let currentNum = nums[i]
+      if (!numsFrequency[currentNum]) {
+        numsFrequency[currentNum] = 0
+      }
+      numsFrequency[currentNum] += 1
+    }
+
+    // creating an array to hold an array of the nums with the frequency as the indices
+    let frequency = new Array(nums.length + 1).fill(0)
+
+    for (let num in numsFrequency) {
+      let freq = numsFrequency[num]
+      if (!frequency[freq]) {
+        frequency[freq] = []
+      }
+      frequency[freq].push(num)
+    }
+
+    console.log(frequency)
+
+    let topKFrequent = []
+    let kCounter = 0
+    for (let i=frequency.length - 1; i>0; i--) {
+      if (frequency[i] == 0) {
+        continue
+      }
+      if (kCounter == k) {
+        return topKFrequent
+      }
+      topKFrequent.push(parseInt(frequency[i]))
+      kCounter++
+      
+    }
+    // sorting the keys by 
 };
 
-console.log(topKFrequent([1,1,1,2,2,3], 2))
+console.log(topKFrequent([1,1,1,2,2,3,3,3,3,4,4], 2))
