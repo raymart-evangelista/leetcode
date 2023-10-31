8da454db4589bb5ade803e6a1860b078d7d46dec
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-10-30 22:02:54 -0700

    decoding working

--- 659_encodeAndDecodeStrings.js
+++ 659_encodeAndDecodeStrings.js
@@ -4,30 +4,53 @@ const encode = (list) => {
   for (let i=0; i<list.length - 1; i++) {
     let charAmt = list[i].length
     let specialChar = '#'
-    let prefix = charAmt + specialChar
-    encoded = encoded + prefix + list[i]
+    let suffix =  charAmt + specialChar
+    encoded = encoded + suffix + list[i]
   }
   return encoded
 
 }
 
 const decode = (str) => {
+  console.log(str)
   // precode a hashmap of all letters in the alphabet,
   // if the char in the string is not a letter, it's the char used to encode
   // so use that char(s) to split the words
-  let strToBeDecoded = str
   let list = []
 
-  // base case is when the string to be decoded is empty
-  if (strToBeDecoded = '') {
-    return list
-  }
-
-  // use a while loop to get the amount of chars to decode as a string
-  let charAmt = []
+  // store into array 
+  let stringLength = undefined
+  let currentString = undefined
+  let stringCounter = undefined
   for (let i=0; i<str.length; i++) {
-    
+    // if the current char is not a hashtag and we aren't 
+    // currently visitng a char that is part of a string
+    // then it's a char that will be part of the number we encoded
+    let currentChar = str[i]
+    if (currentChar != '#' && stringLength == undefined && stringCounter == undefined) {
+      stringLength = currentChar
+    } else if (currentChar != '#' && stringLength && stringCounter == undefined) {
+      stringLength += currentChar
+    } else if  (currentChar == '#' && stringCounter == undefined) {
+      console.log(stringLength)
+      stringCounter = parseInt(stringLength)
+      stringLength = undefined
+    } else {
+      if (currentString == undefined) {
+        currentString = currentChar
+      } else {
+        currentString += currentChar
+      }
+      stringCounter -= 1
+      if (stringCounter == 0) {
+        list.push(currentString)
+        currentString = undefined
+        stringCounter = undefined
+      }
+    }
   }
+  return list
 }
 
-console.log(encode(['lintlintlintlintlintlintlintlintlint', 'code', 'love', 'you']))
+let thingToEncode = ['lintlintlintlintlintlintlintlintlint', 'code', 'love', 'you']
+console.log(decode(encode(thingToEncode)))
