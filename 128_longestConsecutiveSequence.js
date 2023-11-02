6bcfe748796c2aa27808fc054aff6cf8b842de26
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-11-02 00:25:53 -0700

    working solution

--- 128_longestConsecutiveSequence.js
+++ 128_longestConsecutiveSequence.js
@@ -15,6 +15,7 @@ var longestConsecutive = function(nums) {
   }
 
   // for every elem,
+  let highestCount = 0
   for (let i=0; i<nums.length; i++) {
     let element = nums[i]
     let elementBefore = element - 1
@@ -22,19 +23,26 @@ var longestConsecutive = function(nums) {
     // if elem is first elm, count number of elem, 
     //  in the consecutive starting with this elem
     if (hashedNums[elementBefore] == undefined) {
-      hashedNums[element] = 1
+      let counter = 1
+      // hashedNums[element] = 1
       let nextElement = element + 1
         // iterate from next elem, arr[i] + 1 till last elem that can be found,
         while (hashedNums[nextElement] != undefined) {
-          hashedNums[element] += 1
+          // hashedNums[element] += 1
+          counter += 1
           nextElement += 1
+          if (counter > highestCount) {
+            highestCount =  counter
+          }
         }
       }
     }
     // if count is more than previous longest subsequence found, update count
 
-    return hashedNums
+    return highestCount
 };
 
 let arr = [100,4,200,1,3,2,]
-let output = longestConsecutive(arr)
+let arr2 = [0,3,7,2,5,8,4,6,0,1]
+let output = longestConsecutive(arr2)
+console.log(output)
