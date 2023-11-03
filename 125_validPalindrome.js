cca86d72b08c317375c8d2bc5ee91a3bb016e123
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-11-02 23:22:11 -0700

    test code

--- 125_validPalindrome.js
+++ 125_validPalindrome.js
@@ -8,13 +8,17 @@ var isPalindrome = function(s) {
   // remove all non-alphanumeric characters (everything that aren't letters and numbers)
   let alphanumeric = lowercased.replace(/\W/g, '')
 
-  console.log(alphanumeric)
   // meat of the program to check if palindrome
-  console.log(alphanumeric.length)
   let half = parseInt(alphanumeric.length / 2) 
   console.log(half)
   console.log(alphanumeric[half])
+
   // for loop that uses the half length of the string
+  for (let i=0; i<alphanumeric.length / 2; i++) {
+    if (alphanumeric[i] != alphanumeric[alphanumeric.length - 1]) {
+      return false
+    }
+  }
   // pointer that points to start of array, pointer that points to end of array 
   // if at any point the two pointers don't match, return false
   // return true if we make it to the very middle of the string
