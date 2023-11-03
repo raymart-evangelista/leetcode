c84bd0879219bdcb4caa770fbc7a8d56ace4f704
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-11-02 23:25:43 -0700

    working solution

--- 125_validPalindrome.js
+++ 125_validPalindrome.js
@@ -7,25 +7,28 @@ var isPalindrome = function(s) {
   let lowercased = s.toLowerCase()
   // remove all non-alphanumeric characters (everything that aren't letters and numbers)
   let alphanumeric = lowercased.replace(/\W/g, '')
+  alphanumeric = alphanumeric.replace('_', '')
 
   // meat of the program to check if palindrome
   let half = parseInt(alphanumeric.length / 2) 
-  console.log(half)
-  console.log(alphanumeric[half])
 
   // for loop that uses the half length of the string
-  for (let i=0; i<alphanumeric.length / 2; i++) {
-    if (alphanumeric[i] != alphanumeric[alphanumeric.length - 1]) {
-      return false
-    }
-  }
   // pointer that points to start of array, pointer that points to end of array 
   // if at any point the two pointers don't match, return false
   // return true if we make it to the very middle of the string
+  for (let i=0; i<alphanumeric.length / 2; i++) {
+    let startPointer = alphanumeric[i]
+    let endPointer = alphanumeric[alphanumeric.length - 1 - i]
+    if (startPointer != endPointer) {
+      return false
+    }
+  }
+  return true
 };
 
 let s1 = "A man, a plan, a canal: Panama" // true
 let s2 = "race a car" // false
 let s3 = " " // true
+let s4 = "ab_a"
 
-isPalindrome(s1)
+isPalindrome(s4)
