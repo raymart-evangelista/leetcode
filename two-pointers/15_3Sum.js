fb946d5589f95a6b4db2cb9a893ce10b319f873a
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-11-05 23:26:51 -0800

    add base case

---
+++ two-pointers/15_3Sum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    let output = []
+    if (nums.length < 3) {
+        return output
+    }
+    let firstPointer = nums[0]
+    let secondPointer = nums[1]
+    let thirdPointer
+
+
+    
+};
+
+let nums = [-1,0,1,2,-1,-4]
+let nums1 = [0,1]
+
+console.log(threeSum(nums1))
