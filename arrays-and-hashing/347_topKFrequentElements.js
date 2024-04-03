ea122a3a7f03d598c841958f9e6af3d4dc8edc5a
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-04-02 22:37:18 -0700

    new solution

--- arrays-and-hashing/347_topKFrequentElements.js
+++ arrays-and-hashing/347_topKFrequentElements.js
@@ -1,43 +1,28 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number[]}
- */
-var topKFrequent = function(nums, k) {
-    let numsFrequency = {}
-    for (let i=0; i<nums.length; i++) {
-      let currentNum = nums[i]
-      if (!numsFrequency[currentNum]) {
-        numsFrequency[currentNum] = 0
-      }
-      numsFrequency[currentNum] += 1
+// let nums = [1, 1, 1, 2, 2, 5, 5, 5, 5],
+//   k = 2
+
+let nums = [-1, -1],
+  k = 1
+
+const topKFrequent = (nums, k) => {
+  let map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    let currNum = nums[i]
+    if (map.has(currNum)) {
+      map.set(currNum, map.get(currNum) + 1)
+    } else {
+      map.set(currNum, 1)
     }
+  }
 
-    // creating an array to hold an array of the nums with the frequency as the indices
-    let frequency = new Array(nums.length + 1)
+  const sorted = [...map.entries()].sort((a, b) => b[1] - a[1])
 
-    for (let num in numsFrequency) {
-      let freq = numsFrequency[num]
-      if (!frequency[freq]) {
-        frequency[freq] = []
-      }
-      frequency[freq].push(parseInt(num))
-    }
+  let kFreqNums = new Array()
+  for (let i = 0; i < k; i++) {
+    kFreqNums.push(sorted[i][0])
+  }
 
-    console.log(frequency)
+  return kFreqNums
+}
 
-    let topKFrequent = []
-    for (let i=frequency.length - 1; i>=0 && topKFrequent.length < k; i--) {
-      if (frequency[i]) {
-        topKFrequent.push(...frequency[i])
-      }
-      
-    }
-    // sorting the keys by 
-    return topKFrequent
-};
-
-// console.log(topKFrequent([1,1,1,2,2,3,3,3,3,4,4], 2))
-// console.log(topKFrequent([1], 1))
-// console.log(topKFrequent([3,3,4,4], 2))
-console.log(topKFrequent([3,0,1,0], 1))
+topKFrequent(nums, k)
