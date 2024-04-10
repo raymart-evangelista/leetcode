11bf9fb13241cd0614f9ebcea44b82de30e6cf20
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-04-09 20:22:33 -0700

    add solution

--- arrays-and-hashing/238_productOfArrayExceptSelf.js
+++ arrays-and-hashing/238_productOfArrayExceptSelf.js
@@ -1,30 +1,56 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
-var productExceptSelf = function(nums) {
-  let output = []
-  for (let i=0; i<nums.length; i++) {
-    if (output[i-1] != undefined) {
-      output[i] = output[i-1] * nums[i-1]
-    } else {
-      output[i] = 1
-    }
-  }
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// var productExceptSelf = function(nums) {
+//   let output = []
+//   for (let i=0; i<nums.length; i++) {
+//     if (output[i-1] != undefined) {
+//       output[i] = output[i-1] * nums[i-1]
+//     } else {
+//       output[i] = 1
+//     }
+//   }
 
+//   let post = 1
+//   for (let i=nums.length-1; i>= 0; i--) {
+//     if (output[i+1] != undefined) {
+//       post = post * nums[i+1]
+//       output[i] = output[i] * post
+//     } else {
+//       output[i] = output[i] * post
+//     }
+//   }
+
+//   return output
+// };
+
+// // expected output [24,12,8,6]
+// console.log(productExceptSelf([1,2,3,4]))
+
+nums = [1, 2, 3, 4]
+// nums = [-1, 1, 0, -3, 3]
+
+const productExceptSelf = (nums) => {
+  let pre = 1
   let post = 1
-  for (let i=nums.length-1; i>= 0; i--) {
-    if (output[i+1] != undefined) {
-      post = post * nums[i+1] 
-      output[i] = output[i] * post
+  let output = []
+
+  // [1,1,2,6]
+  for (let i = 0; i < nums.length; i++) {
+    if (i == 0) {
+      output[i] = pre
     } else {
-      output[i] = output[i] * post
+      output[i] = pre * nums[i - 1]
+      pre = pre * nums[i - 1]
     }
   }
-  
-
+  for (let i = nums.length - 1; i >= 0; i--) {
+    output[i] = output[i] * post
+    post = post * nums[i]
+  }
+  console.log(output)
   return output
-};
+}
 
-// expected output [24,12,8,6]
-console.log(productExceptSelf([1,2,3,4]))
+productExceptSelf(nums)
