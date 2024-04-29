6fb1021b17df147f5d88244395301f179ace9bc9
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-04-29 11:01:43 -0700

    optimize solution using another array to hold min val

--- stack/155_minStack.js
+++ stack/155_minStack.js
@@ -1,6 +1,6 @@
 var MinStack = function () {
   this.stack = []
-  // this.minStack = []
+  this.minStack = []
   return
 }
 
@@ -10,11 +10,11 @@ var MinStack = function () {
  */
 MinStack.prototype.push = function (val) {
   this.stack.push(val)
-  // val = Math.min(
-  //   val,
-  //   this.minStack.length ? this.minStack[this.minStack.length - 1] : val
-  // )
-  // this.minStack.push()
+  val = Math.min(
+    val,
+    this.minStack.length ? this.minStack[this.minStack.length - 1] : val
+  )
+  this.minStack.push(val)
   return
 }
 
@@ -22,37 +22,22 @@ MinStack.prototype.push = function (val) {
  * @return {void}
  */
 MinStack.prototype.pop = function () {
-  return this.stack.pop()
+  this.stack.pop()
+  this.minStack.pop()
 }
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function () {
-  let i = this.stack.length - 1
-  while (this.stack[i] === null) {
-    i--
-  }
-  return this.stack[i]
+  return this.stack.length ? this.stack[this.stack.length - 1] : null
 }
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function () {
-  let min = this.stack[0]
-  let minIndex = 0
-  for (const key in this.stack) {
-    if (Object.hasOwnProperty.call(this.stack, key)) {
-      const number = this.stack[key]
-      if (number < min) {
-        min = number
-        minIndex = key
-      }
-    }
-  }
-  // this.stack[minIndex] = null
-  return min
+  return this.minStack.length ? this.minStack[this.minStack.length - 1] : null
 }
 
 /**
@@ -63,14 +48,3 @@ MinStack.prototype.getMin = function () {
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
  */
-
-let minStack = new MinStack()
-minStack.push(-2)
-minStack.push(0)
-minStack.push(-1)
-console.log(minStack.getMin())
-console.log(minStack)
-console.log(minStack.top())
-console.log(minStack.pop())
-console.log(minStack)
-console.log(minStack.getMin())
